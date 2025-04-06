14238fbeabacb18558345e970c58910d18052cb1
Manuel G Moran <mmoran002@protonmail.com>
2025-04-05 20:44:56 -0500

    Front End Initial Setup
    Initial Setup Using React + MUI

---
+++ frontend/dataviz-dashboard/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import MainPage from './pages/MainPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/main" element={<MainPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/dataviz-dashboard/src/theme.js
@@ -0,0 +1,22 @@
+// # (Optional) Material UI custom theme setup
+/*
+    This is a multi-line comment.
+    It can be used to explain a block of code
+    or to temporarily disable it.
+*/
+
+ 
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', // Customize as needed
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
+
+export default theme;
