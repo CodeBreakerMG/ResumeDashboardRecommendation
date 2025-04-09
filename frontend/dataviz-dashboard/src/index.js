1f076dab10672f457a6b04ec2f66850140502304
Madelein Villegas <59115375+madeleinvillegas@users.noreply.github.com>
2025-04-09 13:04:51 -0500

    us map with state colored in red

--- frontend/dataviz-dashboard/src/index.js
+++ frontend/dataviz-dashboard/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
+import 'leaflet/dist/leaflet.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
